119fb3a115ffb560f23715c874f3953136a9d2cc
Alvaro Queiroz Pereira <queiroz.85@hotmail.com>
2022-11-23 12:01:29 -0300

    Update ex001.php

--- ex001.php
+++ ex001.php
@@ -5,6 +5,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Primeiro exemplo PHP</title>
+    <style>
+        body{
+            font: normal normal 1em Arial;
+            color:#8a2b54;
+        }
+        
+    </style>
 </head>
 <body>
     <h1>testando PHP</h1>
